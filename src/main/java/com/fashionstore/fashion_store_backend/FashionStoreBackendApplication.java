1d8020d72daf7ec03e7790d01ad7cae7db62870f
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-09-12 18:46:36 +0700

    Initial commit

---
+++ src/main/java/com/fashionstore/fashion_store_backend/FashionStoreBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fashionstore.fashion_store_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FashionStoreBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FashionStoreBackendApplication.class, args);
+	}
+
+}
