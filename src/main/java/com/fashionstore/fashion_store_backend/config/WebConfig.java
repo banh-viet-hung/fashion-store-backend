4798b68c5701d4927eb38539eedd209c96b446ca
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-09-25 22:36:13 +0700

    Cấu hình lại Cors

--- src/main/java/com/fashionstore/fashion_store_backend/config/WebConfig.java
+++ src/main/java/com/fashionstore/fashion_store_backend/config/WebConfig.java
@@ -1,20 +1,23 @@
 package com.fashionstore.fashion_store_backend.config;
 
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class WebConfig implements RepositoryRestConfigurer {
+
+    private static final String FRONT_END_URL = "http://localhost:3000"; // Địa chỉ front-end
 
     @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**") // Áp dụng cho tất cả các endpoint
-                .allowedOrigins("http://localhost:3000")
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH") // Các phương thức HTTP được phép
+    public void configureRepositoryRestConfiguration(RepositoryRestConfiguration config, CorsRegistry cors) {
+        // Cấu hình CORS
+        cors.addMapping("/**") // Cho phép tất cả các đường dẫn
+                .allowedOrigins(FRONT_END_URL) // Chỉ định origin cụ thể
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS") // Các phương thức cho phép
                 .allowedHeaders("*") // Cho phép tất cả các header
-                .allowCredentials(true) // Cho phép gửi cookie hoặc thông tin xác thực
-                .maxAge(3600); // Thời gian cache (1 giờ)
+                .allowCredentials(true); // Cho phép gửi cookie
     }
 }
