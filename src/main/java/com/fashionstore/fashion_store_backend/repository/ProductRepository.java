f5a0bd577380eeeaa66bcf87db159dd5bf3f7f52
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-10-06 16:37:31 +0700

    Search API

--- src/main/java/com/fashionstore/fashion_store_backend/repository/ProductRepository.java
+++ src/main/java/com/fashionstore/fashion_store_backend/repository/ProductRepository.java
@@ -1,10 +1,16 @@
 package com.fashionstore.fashion_store_backend.repository;
 
 import com.fashionstore.fashion_store_backend.model.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RepositoryRestResource(collectionResourceRel = "product", path = "product")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    // Tìm sản phẩm theo tên và phân trang
+    // http://localhost:8080/product/search/findByNameContainingIgnoreCase?name=Áo%20thun%20thể%20thao%20Jacquard%20Seamless&page=0&size=10
+    Page<Product> findByNameContainingIgnoreCase(String name, Pageable pageable);
 }
