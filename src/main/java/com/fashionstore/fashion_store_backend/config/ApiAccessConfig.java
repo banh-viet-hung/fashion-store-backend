380b399a00d97da310ad23e80fb614f82e7cdbad
BANH VIET HUNG <roronoazoro.whois@gmail.com>
2024-12-18 13:15:41 +0700

    reCommit

--- src/main/java/com/fashionstore/fashion_store_backend/config/ApiAccessConfig.java
+++ src/main/java/com/fashionstore/fashion_store_backend/config/ApiAccessConfig.java
@@ -5,7 +5,7 @@ public class ApiAccessConfig {
     // Các API công khai (không cần xác thực)
     public static final String[] PUBLIC_API_GET = {"/cartProduct", "/cartProduct/**", "/category", "/category/**", "/color", "/color/**", "/favoriteProduct", "/favoriteProduct/**", "/feature", "/feature/**", "/feedback", "/feedback/**", "/image", "/image/**", "/paymentMethod", "/paymentMethod/**", "/product", "/product/**", "/role", "/role/**", "/shippingMethod", "/shippingMethod/**", "/size", "/size/**", "/user/check-email", "/user/request-password-reset", "/user/reset-password/**", "/product-variant/quantity/**", "/products/{productId}", "/user/check-status", "/payment/create-payment-url/{orderId}", "/categories/children/{slug}"};
 
-    public static final String[] PUBLIC_API_POST = {"/user/register", "/api/auth/login", "orders/create", "/payment/update-status/{orderId}", "/orders/{orderId}/cancel", "/products/filter", "/user/check-email"};
+    public static final String[] PUBLIC_API_POST = {"/user/register", "/api/auth/login", "orders/create", "/payment/update-status/{orderId}", "/orders/{orderId}/cancel", "/products/filter", "/user/check-email", "/user/request-password-reset", "/user/reset-password/{token}"};
 
     // Các API yêu cầu vai trò ADMIN, STAFF, USER (Tất cả các role có thể truy cập)
     public static final String[] ADMIN_STAFF_USER_API_GET = {"/user/info", "/address", "/user/avatar-and-fullname", "/user/favorite", "/cart/**", "/orders/list", "/orders/{orderId}"};
